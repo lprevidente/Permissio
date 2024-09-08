4b4e8efeb195f23a94291d38a32d8bc018b1d9ea
Bernardo Giordano <bernardogiordano369@gmail.com>
2024-09-08 16:21:01 +0200

    Remove BaseEntity extension from Group. Allows generic base entities and group entities to have different ID types

--- src/main/java/com/lprevidente/permissio/entity/Group.java
+++ src/main/java/com/lprevidente/permissio/entity/Group.java
@@ -2,7 +2,7 @@ package com.lprevidente.permissio.entity;
 
 import java.util.Collection;
 
-public interface Group<T extends BaseEntity<ID>, ID> extends BaseEntity<ID> {
+public interface Group<T extends BaseEntity<ID>, ID> {
 
   Collection<T> getMembers();
 }
