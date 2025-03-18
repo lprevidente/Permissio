893f493d312bfcf3a43e46c52026696186cefc66
Luigi Previdente <lprevidente@gmail.com>
2025-03-18 22:16:40 +0100

    Github Action

--- src/test/java/com/lprevidente/permissio/serialization/SerializationTest.java
+++ src/test/java/com/lprevidente/permissio/serialization/SerializationTest.java
@@ -19,6 +19,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @EnableAcRepositories("com.lprevidente.permissio.repository")
 class SerializationTest {
 
+
+
   private final ObjectMapper mapper;
 
   public SerializationTest() {
